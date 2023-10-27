cba65a2f787b57677c5c72cf60c138b840185a41
oozoemenam <oozoemenam@gmail.com>
2023-10-27 18:58:22 +0200

    item list

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="home">
+    <items-list :items="items" @select-item="onSelectItem"></items-list>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import ItemsList from './items/components/ItemsList.vue';
+import { Item } from './items/models/Item';
+
+const items: Item[] = reactive([
+  { id: 1, name: 'Item 1', selected: false },
+  { id: 2, name: 'Item 2', selected: false },
+  { id: 3, name: 'Item 3', selected: false }
+]);
+
+const onSelectItem = (id: number) => {
+  const item = items.find((item) => item.id === id);
+  if (!item) {
+    return console.log(`onSelectItem: could not find item with id: ${id}`);
+  }
+  item.selected = !item.selected;
+  console.log('onSelectItem', item.id, item.selected);
+};
+</script>
+
+<style></style>
---
+++ src/items/components/ItemsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>Items:</h3>
+  <ul>
+    <li v-for="item in items" :key="item.id" @click="handleClick(item)">
+      {{ item.name }} [{{ item.selected }}]
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { Item } from '../models/Item';
+
+defineProps<{ items: Item[] }>();
+
+const emit = defineEmits<{
+  (e: 'selectItem', id: number): any;
+}>();
+
+const handleClick = (item: Item) => {
+  emit('selectItem', item.id);
+};
+</script>
+
+<style></style>
